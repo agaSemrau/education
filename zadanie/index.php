adb641c4b50a97d970db9d44f7a99a60f5001540
asemrau <agnieszka.semrau@wp.pl>
2016-08-18 11:16:38 +0200

    final commit

--- zadanie/index.php
+++ zadanie/index.php
@@ -7,3 +7,4 @@ $twig = new Twig_Environment($loader, array('cache' => false));
 $pageName = !empty($_GET['page']) && preg_match('/^[a-zA-Z]+$/', $_GET['page']) ? $_GET['page'] : 'index';
 $template = $twig->loadTemplate("$pageName.twig");
 echo $template->render(json_decode(file_get_contents('data.json'), true));
+
